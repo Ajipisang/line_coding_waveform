aee930606b785990b1cd1d0aa38f8e8e633198e1
ajipisang <arzaqvirley777@gmail.com>
2025-06-19 18:28:15 +0700

    revisi 64 bit

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
 from  line_coding_waveform.menu.show_menu import  choice_menu
-
+def convert_bit(id):
+    total=str(id) + "151362"
+    binary_4bit = ''.join([format(int(digit), '04b') for digit in total])
+    return  binary_4bit
 def main():
     while True:
         try:
             user_input = int(input("input angka yang ingin di simulasikan (mis : 1513623040) >> "))
             # conversi angka yang diinput user ke biner
-            covert_biner = format(user_input, "032b")
-            print(covert_biner)
+            covert_biner = convert_bit(user_input)
+            print(len(covert_biner))
             choice_menu(covert_biner,user_input)
         except ValueError:
             print("input hanya boleh angka !!")
